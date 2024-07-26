2e6864785ec48927169f5aab352306724e7afd74
Ali <2153970@cognizant.com>
2024-07-26 15:49:49 +0530

    stopwatch commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,10 @@
-import './App.css'
-import { CarList } from './CarList'
-import { Counter } from './Counter'
-import { DigitalClock } from './DigitalClock'
-import { List } from './List'
-import { Todo } from './Todo'
-import { UserGreeting } from './UserGreeting'
+import { Stopwatch } from './Stopwatch'
 
 function App() {
 
     return (
         <div>
-            {/* <UserGreeting isLoggedIn={true} username="Zidan" /> */}
-            {/* <List></List> */}
-            {/* <Counter></Counter> */}
-            {/* <CarList></CarList> */}
-            {/* <Todo></Todo> */}
-            <DigitalClock></DigitalClock>
+            <Stopwatch></Stopwatch>
         </div>
     )
 }
